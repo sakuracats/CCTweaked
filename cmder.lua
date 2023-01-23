2b8133d346f630a254f9b2f57db667b060222c06
sakuracats <87510234+sakuracats@users.noreply.github.com>
2023-01-23 19:25:09 +0800

    Update cmder.lua

--- cmder.lua
+++ cmder.lua
@@ -1,17 +1,18 @@
 local gen1 = rednet.lookup("im_gen","gen1")
 local gen2 = rednet.lookup("im_gen","gen2")
 
-local monitor1 = rednet.lookup("monitor","monitor1")
+monitor1 = rednet.lookup("monitor","monitor1")
 
 rednet.host( "cmder", "cmder1" )
 rednet.send(gen1,false)
 
 while true do
     local id,message = rednet.receive()
+        msg = {message[1],message[2],message[3]}
         if message[1] == "Running" then
-            rednet.send(monitor1,message)
-        elseif messagep[1] == "Stopped" then
-            rednet.send(monitor1,message)
+            rednet.send(monitor1,msg)
+        elseif message[1] == "Stopped" then
+            rednet.send(monitor1,msg)
         else
         end
 end
