15e6acfdf7d18999995404097e642803e9861e18
sakuracats <87510234+sakuracats@users.noreply.github.com>
2023-01-23 14:19:41 +0800

    Update gen.lua

--- gen.lua
+++ gen.lua
@@ -1,8 +1,8 @@
 m = peripheral.wrap("back")
 m.clear()
 m.setTextScale(1)
-m.setTextColor(colors.red)
-
+m.setTextColor(colors.white)
+m.setBackgroundColor(colors.black)
 width,height = m.getSize()
 row = height/10
 col = width/2
@@ -12,6 +12,12 @@ function gen(m)
     m.setCursorPos(0+1,0+row*2)
     m.write("gen1")
     m.setCursorPos(0+1+col,0+row*2)
-    m.write("running")
+    m.setBackgroundColor(colors.green)
+    m.write("Running")
+    m.setCursorPos(0+1,0+row*3)
+    m.write("gen2")
+    m.setCursorPos(0+1+col*2,0+row*3)
+    m.setBackgroundColor(colors.red)
+    m.write("Stopped")
 end
 gen(m)
